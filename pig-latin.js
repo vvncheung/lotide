1dc7e9ac7f561ea9a513a0faabbf3be758526a2d
Vivian Cheung <hi@viviancheung.ca>
2021-05-25 23:38:03 +0000

    cleaning

---
+++ pig-latin.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const args = process.argv;
+let array = args.slice(2);
+
+const pigLatinatorOneWord = function(string) {
+  let pigLatinedString = "";
+  let editedString = string.slice(1);
+  pigLatinedString = editedString + string.charAt(0) + "ay";
+  return pigLatinedString;
+};
+
+const pigLatinator = function(string) {
+  let finalString = "";
+  for (let i = 0; i < string.length; i++) {
+    finalString += pigLatinatorOneWord(string[i]) + " ";
+  }
+  console.log(finalString);
+};
+
+pigLatinator(array);
+
---
+++ reverse.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const args = process.argv;
+let array = args.slice(2);
+
+const reverseString = function(string) {
+  for (let j = 0; j < string.length; j++) {
+    let reversedString = "";
+    for (let i = string[j].length - 1; i >= 0; i--) {
+      reversedString = reversedString + string[j][i];
+    }
+    console.log(reversedString);
+  }
+};
+
+reverseString(array);
