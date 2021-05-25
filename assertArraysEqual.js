24db2ac8c5a2ce59b7f3992b64b7beade80a2aa9
Vivian Cheung <hi@viviancheung.ca>
2021-05-25 23:37:16 +0000

    creating function to compare two arrays through assertion

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    // console.log(`🙇🏻‍♀️Assertion passed:" ${actual} === ${expected}`);
+    return;
+  } else {
+    console.log(`🙅🏻‍♀️Assertion failed: ${actual} !==  ${expected}`);
+    return;
+  }
+};
