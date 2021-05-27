50b04adcfd77bfcc72e16ee4134b846e8ad5bf53
Vivian Cheung <hi@viviancheung.ca>
2021-05-27 01:51:27 +0000

    adding countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🙇🏻‍♀️Assertion passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
+    return;
+  } else {
+    console.log(`🙅🏻‍♀️Assertion failed: ${JSON.stringify(actual)} !==  ${JSON.stringify(expected)}`);
+    return;
+  }
+};
+
+const countLetters = function(string) {
+  
+  const results = {};
+
+  for (let letter of string) {
+    if (letter !== '') {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+console.log(countLetters('Lighthouse in the house'));
