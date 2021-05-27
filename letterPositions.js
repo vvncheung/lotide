17da268263652d34f00ba8e78147f143fc1acceb
Vivian Cheung <hi@viviancheung.ca>
2021-05-27 02:49:14 +0000

    adding letterPositions.js to lotide

---
+++ letterPositions.js
@@ -0,0 +1,41 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🙇🏻‍♀️Assertion passed:" ${actual} === ${expected}`);
+    return;
+  } else {
+    console.log(`🙅🏻‍♀️Assertion failed: ${actual} !==  ${expected}`);
+    return;
+  }
+};
+
+
+const letterPositions = function(sentence) {
+
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== '') {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i)
+      } else {
+        results[sentence[i]] = [i]
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
