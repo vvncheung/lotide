85eeec6cc5e6d1f896ce3d4f40a4bdb081d7d206
Vivian Cheung <hi@viviancheung.ca>
2021-05-28 02:26:29 +0000

    adding findKey.js to lotide

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return console.log(`🙅🏻‍♀️Assertion failed: ${actual} !==  ${expected}`);
+  } else {
+    return console.log(`🙇🏻‍♀️Assertion passed:" ${actual} === ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  let result = "";
+  for (const key in object) {
+    if (callback(object[key])) {
+      result = key;
+      return result;
+    } else {
+      result = undefined;
+    }
+  }
+  return result;
+};
+
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+assertEqual(result, "noma")
