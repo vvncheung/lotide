125d05e07d86db0efea94db2fe386e884384b11e
Vivian Cheung <hi@viviancheung.ca>
2021-05-28 02:11:28 +0000

    adding takeUntil.js to lotide

---
+++ takeUntil.js
@@ -0,0 +1,47 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🙇🏻‍♀️Assertion passed:" ${actual} === ${expected}`);
+    return;
+  } else {
+    console.log(`🙅🏻‍♀️Assertion failed: ${actual} !==  ${expected}`);
+    return;
+  }
+};
+
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (let index of array) {
+    if (!callback(index)) {
+      result.push(index);
+    } else {
+      break;
+    }
+  }
+  return result;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
