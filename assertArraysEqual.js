40681d75bf2f43067304b83891432e0575fad7d5
Vivian Cheung <hi@viviancheung.ca>
2021-05-31 18:41:03 +0000

    refactored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -20,3 +9,5 @@ const assertArraysEqual = function(actual, expected) {
     return;
   }
 };
+
+module.exports = assertArraysEqual;
